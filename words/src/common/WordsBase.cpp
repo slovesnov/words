425691b705e1aff8f73a53127ac7771fac7e2b7d
aleksey slovesnov <slovesnov@yandex.ru>
2024-03-17 10:13:39 +0300

    1

--- words/src/common/WordsBase.cpp
+++ words/src/common/WordsBase.cpp
@@ -1917,7 +1917,7 @@ bool WordsBase::findLetterGroupSplit() {
 	m_addstatus = "";
 	for (i = 0; i < 2; i++) {
 		m_addstatus += m_language[i ? TRIPLETS : PAIRS] + " "
-				+ toString(n[i], m_language[SEPARATOR_SYMBOL][0]) + " ";
+				+ intToStringLocaled(n[i]) + " ";
 	}
 
 	return false;
